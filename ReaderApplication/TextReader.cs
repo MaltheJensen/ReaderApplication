3146bf93f80427985f5d51b68f00ac58837d4564
DataMate93 <70841862+DataMate93@users.noreply.github.com>
2023-01-05 07:16:15 +0100

    Add project files.

---
+++ ReaderApplication/TextReader.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Speech.Synthesis;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReaderApplication
+{
+    public class TextReader
+    {
+        SpeechSynthesizer synth;
+        public TextReader()
+        {
+            synth = new SpeechSynthesizer();
+        }
+        public void ReadText()
+        {
+            TextGetter textGetter = new TextGetter();
+            synth.SpeakAsync(textGetter.GetText());
+        }
+
+        public void CancelReading() 
+        {
+            synth.SpeakAsyncCancelAll();
+        }
+
+        public void SetReadingSpeed(int speed) 
+        {
+            synth.Rate = SelectSpeed(speed);
+        }
+        private int SelectSpeed(int speed) 
+        {
+            int result = 0;
+
+            switch (speed)
+            {
+                case 10:
+                    result = 10;
+                    break;
+                case 9:
+                    result = 8;
+                    break;
+                case 8:
+                    result = 6;
+                    break;
+                case 7:
+                    result = 4;
+                    break;
+                case 6:
+                    result = 2;
+                    break;
+                case 5:
+                    result = 0;
+                    break;
+                case 4:
+                    result = -2;
+                    break;
+                case 3:
+                    result = -4;
+                    break;
+                case 2:
+                    result = -6;
+                    break;
+                case 1:
+                    result = -8;
+                    break;
+            }
+            return result;
+        }
+
+        public void VoiceSetter()
+        {
+
+        }
+    }
+}
---
+++ ReaderApplication/UserSettings.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using ReaderApplication.Properties;
+
+namespace ReaderApplication
+{
+    /// <summary>
+    /// Interaction logic for Settings.xaml
+    /// </summary>
+    public partial class UserSettings : Window
+    {
+        public UserSettings()
+        {
+            InitializeComponent();
+            Loaded += OnStart;
+        }
+        private void OnStart(object sender, System.EventArgs e) 
+        {
+            var numb = (double)Properties.Settings.Default["VoiceSpeed"];
+            ReadingSpeedSlider.Value = numb;
+        }
+
+        private void RadioButton_Checked_Male_1(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("you clicked male button 1");
+        }
+
+        private void RadioButton_Checked_Male_2(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("you clicked male button 2");
+        }
+
+        private void RadioButton_Checked_Female_1(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("you clicked female button 1");
+        }
+
+        private void RadioButton_Checked_Female_2(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("you clicked female button 2");
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            Properties.Settings.Default["VoiceSpeed"] = ReadingSpeedSlider.Value;
+            Properties.Settings.Default.Save();
+        }
+
+    }
+}
